a51764601f8170a8f645ebbf0e15e5313f57f5a7
Mandil <mallamandilcoding@gmail.com>
2024-02-02 23:40:07 +0900

    logout admin

--- basic/routes/web.php
+++ basic/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::controller(AdminController::class)->group(function () {
+    Route::get('admin/logout', 'destroy')->name('admin.logout');
 });
 
 require __DIR__.'/auth.php';
